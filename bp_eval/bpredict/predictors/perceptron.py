bdc8fc3d7b019a14c932e793a9350e0464b2d7a6
Alexander Fasching <fasching.a91@gmail.com>
2019-01-20 19:28:21 +0100

    Add perceptron predictor with local history
    Change-Id: Icdd61d1fb0129d4567ac114919906201dc3125f5

--- bp_eval/bpredict/predictors/perceptron.py
+++ bp_eval/bpredict/predictors/perceptron.py
@@ -20,7 +20,7 @@ Perceptron predictor suitable for hardware implementation. Only relies on
 saturating integer arithmetic.
 """
 
-__all__ = ('PerceptronPredictor', )
+__all__ = ('PerceptronPredictor', 'LocalPerceptronPredictor')
 
 import numpy as np
 from ..basepredictor import BasePredictor
@@ -114,6 +114,78 @@ class PerceptronPredictor(BasePredictor):
 
         self._globalhistory = np.roll(self._globalhistory, -1)
         self._globalhistory[-1] = t
+
+    def _get_index(self, branch_addr):
+        return ((branch_addr // 4) % len(self._table))
+
+
+class LocalPerceptronPredictor(BasePredictor):
+    """A perceptron predictor using the local branch history with optional
+    support for a speculative history.
+    If speculation is enabled, the predictor tracks its predictions and uses
+    them for further predictions. The weights are only updated when the outcome
+    of a branch is known.
+    """
+    def __init__(self, nperceptrons, histlength, threshold=1.0, clip=np.infty,
+                 speculative=False):
+        super(LocalPerceptronPredictor, self).__init__()
+        self._nperceptrons = nperceptrons
+        self._histlength = histlength
+        self._histories = [np.zeros(histlength) for _ in range(nperceptrons)]
+
+        self._table = [Perceptron(histlength, threshold=threshold, clip=clip)
+                            for _ in range(nperceptrons)]
+
+        # The predictor tracks predictions not yet commited (update not called)
+        # and bases speculative predictions based on this temporary history.
+        # Assume that the speculative history is unbounded for simplicity.
+        self._speculative = speculative
+        self._spechistory = [[] for _ in range(nperceptrons)]
+
+    def lookup(self, tid, branch_addr, bp_history):
+        index = self._get_index(branch_addr)
+
+        hist = self._histories[index]
+        if self._speculative:
+            temphist = np.concatenate((hist, self._spechistory[index]))
+            hist = temphist[-self._histlength:]
+
+        p =  self._table[index].predict(hist)
+
+        if self._speculative:
+            self._spechistory[index].append(1 if p else -1)
+
+        return p
+
+    def btb_update(self, tid, branch_addr, bp_history):
+        """Set the outcome of the last speculative prediction to not taken."""
+        if bp_history['conditional'] and self._speculative:
+            index = self._get_index(branch_addr)
+            self._spechistory[index][-1] = -1
+
+    def squash(self, tid, bp_history):
+        """Squashing starts at the tip of the current path, so we remove the
+        last element from the speculative history.
+        """
+        # TODO: This is only called for the MinorCPU?
+        if bp_history['conditional'] and self._speculative:
+            index = self._get_index(branch_addr)
+            self._spechistory[index].pop()
+
+    def update(self, tid, branch_addr, taken, bp_history, squashed):
+        # Ignore the squashed update call or unconditional branches
+        if squashed or not bp_history['conditional']:
+            return
+
+        index = self._get_index(branch_addr)
+        t = 1 if taken else -1
+        self._table[index].update(self._histories[index], t)
+
+        if self._speculative:
+            self._spechistory[index].pop(0)
+
+        self._histories[index] = np.roll(self._histories[index], -1)
+        self._histories[index][-1] = t
 
     def _get_index(self, branch_addr):
         return ((branch_addr // 4) % len(self._table))
