1eec8c0029c14ea97b409aa286633c6abffabd9d
Alexander Fasching <fasching.a91@gmail.com>
2019-01-23 10:00:28 +0100

    Cleanup for submission
    Change-Id: I5ae8c11b7375b7904ae3c17d0f45f9cb9ebf5e59

--- bp_eval/build-benchmarks.sh
+++ bp_eval/build-benchmarks.sh
@@ -10,13 +10,10 @@ BENCHMARKS_DIR=$SCRIPT_DIR/benchmarks
 mkdir -p $DOWNLOAD_DIR && cd $DOWNLOAD_DIR
 
 # Get the source archives of the benchmark applications
-wget -nc 'http://eddylab.org/software/hmmer/hmmer-2.3.2.tar.gz'
 wget -nc 'http://fmv.jku.at/picosat/picosat-965.tar.gz'
 wget -nc 'https://ftp.gnu.org/gnu/coreutils/coreutils-8.1.tar.gz'
 wget -nc 'http://parsec.cs.princeton.edu/download/3.0/parsec-3.0-core.tar.gz'
-wget -nc 'https://github.com/official-stockfish/Stockfish/archive/sf_2.3.1.tar.gz'
 wget -nc 'https://www.sjeng.org/ftp/Sjeng-Free-11.2.tar.gz'
-wget -nc 'ftp://ftp.gnu.org/gnu/gdbm/gdbm-1.18.1.tar.gz'
 
 # Build the benchmark applications if they haven't been built before
 export PATH=$TOOLCHAIN_DIR/alphaev67-unknown-linux-gnu/bin:$PATH
@@ -24,14 +21,6 @@ mkdir -p $BUILD_DIR
 
 cd $BENCHMARKS_DIR/hello-world
 alphaev67-unknown-linux-gnu-gcc -Wall -static -o hello-world hello-world.c
-
-cd $BUILD_DIR
-if [ ! -d hmmer-2.3.2 ]; then
-    tar xf "$DOWNLOAD_DIR/hmmer-2.3.2.tar.gz" && cd hmmer-2.3.2
-    ./configure --host=alphaev67-unknown-linux-gnu
-    make CFLAGS='-static -O2' -j3
-    cp src/hmmsearch $BENCHMARKS_DIR/hmmr/
-fi
 
 cd $BUILD_DIR
 if [ ! -d picosat-965 ]; then
@@ -61,15 +50,6 @@ if [ ! -d parsec-3.0 ]; then
     cd $BUILD_DIR/parsec-3.0/pkgs/kernels/streamcluster/src
     make CXX=alphaev67-unknown-linux-gnu-g++ CXXFLAGS='-static -O2'
     cp streamcluster $BENCHMARKS_DIR/streamcluster/
-fi
-
-cd $BUILD_DIR
-if [ ! -d Stockfish-sf_2.3.1 ]; then
-    tar xf "$DOWNLOAD_DIR/sf_2.3.1.tar.gz" && cd Stockfish-sf_2.3.1/src
-    make build EXTRACXXFLAGS+=-static \
-               EXTRALDFLAGS=-static \
-               CXX=alphaev67-unknown-linux-gnu-g++ COMP=gcc ARCH=general-64 -j3
-    cp stockfish $BENCHMARKS_DIR/stockfish/
 fi
 
 cd $BUILD_DIR
